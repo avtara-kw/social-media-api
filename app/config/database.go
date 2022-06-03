1c510d6db5efb34679b1c7528e221b6282ad8989
Muhammad Avtara Khrisna <avtara.khrisna@koinworks.com>
2022-06-03 22:20:23 +0700

    feat: added config database and gin

--- app/config/database.go
+++ app/config/database.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"os"
+	"time"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	DbUser := os.Getenv("DB_USER")
+	DbPass := os.Getenv("DB_PASS")
+	DbHost := os.Getenv("DB_HOST")
+	DbPort := os.Getenv("DB_PORT")
+	DbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", DbHost, DbPort, DbUser, DbPass, DbName)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxIdleTime(10 * time.Second)
+	db.SetConnMaxLifetime(10 * time.Second)
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main()  {
+import (
+	"github.com/avtara-kw/social-media-api/app/config"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	_, err := config.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	port := ":" + os.Getenv("PORT")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Println("server running at port ", port)
+	router.Run(port)
 }
